07e32bf9411d11db8c54212f384f79cea9a95fb6
Eugene Tsurcan <eugene.tsurcan@gmail.com>
2014-06-11 20:15:30 +0300

    ...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
    $("[data-toggle='tooltip']").tooltip(); 
+   
+   var peopleScroll = new Swiper('#people-scroll .swiper-container', {
+    scrollContainer:true,
+    mousewheelControl : true,
+    mode:'vertical',
+    //Enable Scrollbar
+    scrollbar: {
+      container :'#people-scroll .swiper-scrollbar',
+      hide: false,
+      draggable: true
+    }
+  });
+  
+  var rightContentScroll = new Swiper('#right-content-scroll .swiper-container', {
+    scrollContainer:true,
+    mousewheelControl : true,
+    mode:'vertical',
+    //Enable Scrollbar
+    scrollbar: {
+      container :'#right-content-scroll .swiper-scrollbar',
+      hide: false,
+      draggable: true
+    }
+  });     
 });
